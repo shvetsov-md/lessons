ed5cca12741b17b447898886ec98933dfa2c357f
shvetsov-md <shvetsov-md@dartit.ru>
2021-07-07 20:48:13 +0500

    3

--- Lessons/lesson_1/homework_1_3.swift
+++ Lessons/lesson_1/homework_1_3.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-func homework_1_3() {
+func homework_1_3(sum: Double, percent: Double, year: Double) {
+   
+    var oneYear = 1 + percent/100
     
+    var T = sum * pow(oneYear, year)  // cумма вклада через указанное количество лет
+    
+    print ("Сумма вклада через \(Int(year)) лет: \(String(format: "%.2f", T))")
 }
--- Lessons/main.swift
+++ Lessons/main.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-//homework_1_1() // Урок 1. Задание 1
-homework_1_2() // Урок 1. Задание 2
-
+homework_1_1() // Урок 1. Задание 1
+//homework_1_2() // Урок 1. Задание 2
+//homework_1_3 (sum: 3000, percent: 12, year: 5) // Урок 1. Задание 3
 
 
