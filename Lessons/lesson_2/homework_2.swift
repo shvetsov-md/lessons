1b6060bf02b576f6728966dbe9bd859bbd716ed3
shvetsov-md <shvetsov-md@dartit.ru>
2021-07-12 23:00:44 +0500

    lesson_2

--- Lessons/lesson_2/homework_2.swift
+++ Lessons/lesson_2/homework_2.swift
@@ -1,8 +1,66 @@
-//
-//  homework_2.swift
-//  Lessons
-//
-//  Created by Максим Швецов on 07.07.2021.
-//
+
 
 import Foundation
+
+
+//Задание 1
+
+func homework_2_1(chislo: Double) {
+    if (floor(chislo) == chislo) && (Int(chislo) % 2 == 0) {
+        print ("\(chislo): четное число")
+    }
+    else {
+        print ("\(chislo): не четное число")
+    }
+}
+
+
+//Задание 2
+
+func homework_2_2(chislo: Double) {
+    switch chislo {
+    case _ where (floor(chislo) == chislo) && (Int(chislo) % 3 == 0):
+        let result = chislo / 3
+        print ("\(chislo) делится на три")
+        print ("Результат: \(result)")
+        
+    default:
+        print ("\(chislo) не делится на три без остатка")
+    
+    }
+}
+
+
+
+//Задание 3, 4
+
+func homework_2_3() -> [Int] {
+    var numbers = [Int]()
+    
+    for i in 1...100 {
+        numbers.append(i)
+    }
+    
+    return numbers
+}
+
+
+//Задание 4
+
+func homework_2_4(numbers: [Int]) -> [Int] {
+    var i = 0
+    var arrayResult = numbers
+    
+    while i < numbers.count {
+        if (numbers[i] % 2 == 0) || (numbers[i] % 3 != 0) {
+            arrayResult.removeAll(where:{ $0 == numbers[i] })
+        }
+        
+        i += 1
+    }
+    
+    return arrayResult
+}
+
+
+
--- Lessons/main.swift
+++ Lessons/main.swift
@@ -1,7 +1,28 @@
 import Foundation
 
-homework_1_1() // Урок 1. Задание 1
+//Урок 1
+
+//homework_1_1() // Урок 1. Задание 1
 //homework_1_2() // Урок 1. Задание 2
 //homework_1_3 (sum: 3000, percent: 12, year: 5) // Урок 1. Задание 3
 
 
+
+//Урок 2
+print ("Задание 1:")
+homework_2_1(chislo: 16) // Урок 2. Задание 1
+print ("\n")
+
+print ("Задание 2:")
+homework_2_2(chislo: 27) // Урок 2. Задание 2
+print ("\n")
+
+homework_2_3() // Урок 2. Задание 3
+print ("Задание 3:")
+print ("Массив: \(homework_2_3())")
+print ("\n")
+
+homework_2_4(numbers: homework_2_3()) // Урок 2. Задание 4
+print ("Задание 4:")
+print ("Массив: \(homework_2_4(numbers: homework_2_3()))")
+print ("\n")
