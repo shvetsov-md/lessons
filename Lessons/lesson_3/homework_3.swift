6488bb2931cd1752fac20bbe0e1291ba10ba02ba
shvetsov-md <shvetsov-md@dartit.ru>
2021-07-18 18:33:08 +0500

    3

--- Lessons/lesson_3/homework_3.swift
+++ Lessons/lesson_3/homework_3.swift
@@ -26,21 +26,20 @@ func homework_3() -> () {
             case close = "Закрыть окно"
         }
         
-        enum Trunk {
-            case load (litr: Double)        //загруженный объем
-            case upload (litr: Double)      //выгруженный объем
-            case volume (litr: Double)      //общий объем
-            case fillVilume (litr: Double)  //заполненный объем
+        enum ActionTrunk {
+            case load        //загрузука багажника
+            case upload      //выгрузка багажника
         }
+        
     }
     
     struct SportCar {
         var brand: String
-        var years: Date
-        var volumeTrunck: ActionsCars.Trunk
+        var years: String
+        var volumeTrunck: Double  //Общий объем багажника
         var engine: ActionsCars.Engine
         var window: ActionsCars.Windows
-        var fillVolumeTrunck: ActionsCars.Trunk
+        var fillVolumeTrunck: Double //Заполненный объем багажника
         
         mutating func actionEngine (action: ActionsCars.ActionEngine) {
             switch action {
@@ -63,15 +62,39 @@ func homework_3() -> () {
                 print(window.rawValue)
             }
         }
+         
+        mutating func editVolumeTrunck (action: ActionsCars.ActionTrunk, value: Double) {     //Изменение объема багажника
+            
+            switch action {
+            case .load where fillVolumeTrunck + value <= volumeTrunck:
+                self .fillVolumeTrunck += value
+                print("Заполненный объем багажника: \(fillVolumeTrunck)")
+            case .load where fillVolumeTrunck + value > volumeTrunck:
+                print("Указанное значение не помещается в багажник")
+            
+            case .upload where fillVolumeTrunck >= value:
+                self .fillVolumeTrunck -= value
+                print("Заполненный объем багажника: \(fillVolumeTrunck)")
+            case .upload where fillVolumeTrunck < value:
+                print("Указанное значение превышает заполненный объем багажника")
+            default:
+            break
+            }
+        }
+        
+        func description() {
+            print("Модель \(brand) \(years) года выпуска, с объемом багажника \(volumeTrunck)")
+        }
+        
     }
     
     struct TrunkCar {
         var brand: String
-        var years: Date
-        var volumeTrunck: ActionsCars.Trunk
+        var years: String
+        var volumeTrunck: Double
         var engine: ActionsCars.Engine
         var window: ActionsCars.Windows
-        var fillVolumeTrunck: ActionsCars.Trunk
+        var fillVolumeTrunck: Double
         
         mutating func actionEngine (action: ActionsCars.ActionEngine) {
             switch action {
@@ -94,9 +117,79 @@ func homework_3() -> () {
                 print(window.rawValue)
             }
         }
+        
+        mutating func editVolumeTrunck (action: ActionsCars.ActionTrunk, value: Double) {     //Изменение объема багажника
+            
+            switch action {
+            case .load where fillVolumeTrunck + value <= volumeTrunck:
+                self .fillVolumeTrunck += value
+                print("Заполненный объем багажника: \(fillVolumeTrunck)")
+            case .load where fillVolumeTrunck + value > volumeTrunck:
+                print("Указанное значение не помещается в багажник")
+            
+            case .upload where fillVolumeTrunck >= value:
+                self .fillVolumeTrunck -= value
+                print("Заполненный объем багажника: \(fillVolumeTrunck)")
+            case .upload where fillVolumeTrunck < value:
+                print("Указанное значение превышает заполненный объем багажника")
+            default:
+            break
+            }
+        }
+        
+        func description() {
+            print("Перед осмотром модели \(brand), необходимо \(engine.rawValue) и \(window.rawValue)")
+        }
     }
     
+    var spotCar_1 = SportCar(brand: "Hyundai", years: "2018", volumeTrunck: 370, engine: .start, window: .close, fillVolumeTrunck: 150)
+    var spotCar_2 = SportCar(brand: "Volkswagen", years: "2016", volumeTrunck: 450, engine: .stop, window: .open, fillVolumeTrunck: 150)
     
+    var trunckCar_1 = TrunkCar(brand: "Kamaz", years: "2010", volumeTrunck: 1500, engine: .start, window: .close, fillVolumeTrunck: 650)
+    var trunckCar_2 = TrunkCar(brand: "Peterbilt", years: "2005", volumeTrunck: 1800, engine: .stop, window: .open, fillVolumeTrunck: 1050)
+    
+    //Запус/остановка двигателя
+    print("Запус/остановка двигателя:")
+    print(spotCar_1.brand, spotCar_1.years)
+    spotCar_1.actionEngine(action: .start)
+    print(spotCar_2.brand, spotCar_2.years)
+    spotCar_2.actionEngine(action: .stop)
+    print(trunckCar_1.brand, trunckCar_1.years)
+    trunckCar_1.actionEngine(action: .stop)
+    print(trunckCar_2.brand, trunckCar_2.years)
+    trunckCar_2.actionEngine(action: .start)
+    print("\n")
+    
+    //Открытие/закрытие окна
+    print("Открытие/закрытие окна:")
+    print(spotCar_1.brand, spotCar_1.years)
+    spotCar_1.actionWindow(action: .close)
+    print(spotCar_2.brand, spotCar_2.years)
+    spotCar_2.actionWindow(action: .open)
+    print(trunckCar_1.brand, trunckCar_1.years)
+    trunckCar_1.actionWindow(action: .open)
+    print(trunckCar_2.brand, trunckCar_2.years)
+    trunckCar_2.actionWindow(action: .close)
+    print("\n")
+    
+    //Загрузка/выгрузка багажника
+    print("Загрузка/выгрузка багажника:")
+    print(spotCar_1.brand, spotCar_1.years)
+    spotCar_1.editVolumeTrunck(action: .load, value: 150)
+    print(spotCar_2.brand, spotCar_2.years)
+    spotCar_2.editVolumeTrunck(action: .upload, value: 200)
+    print(trunckCar_1.brand, trunckCar_1.years)
+    trunckCar_1.editVolumeTrunck(action: .load, value: 900)
+    print(trunckCar_2.brand, trunckCar_2.years)
+    trunckCar_2.editVolumeTrunck(action: .upload, value: 650)
+    print("\n")
+    
+    //Описание
+    print("Описание:")
+    spotCar_1.description()
+    spotCar_2.description()
+    trunckCar_1.description()
+    trunckCar_2.description()
     
 }
 
--- Lessons/main.swift
+++ Lessons/main.swift
@@ -10,32 +10,37 @@ import Foundation
 
 //Урок 2
 
-// Урок 2. Задание 1
-print ("Задание 1:")
-homework_2_1(chislo: 16)
-print ("\n")
+//// Урок 2. Задание 1
+//print ("Задание 1:")
+//homework_2_1(chislo: 16)
+//print ("\n")
+//
+//// Урок 2. Задание 2
+//print ("Задание 2:")
+//homework_2_2(chislo: 27)
+//print ("\n")
+//
+//// Урок 2. Задание 3
+//print ("Задание 3:")
+//print ("Массив: \(homework_2_3())")
+//print ("\n")
+//
+//// Урок 2. Задание 4
+//print ("Задание 4:")
+//print ("Массив: \(homework_2_4(numbers: homework_2_3()))")
+//print ("\n")
+//
+//// Урок 2. Задание 5
+//print ("Задание 5:")
+//print ("Массив: \(homework_2_5(50))")
+//print ("\n")
+//
+//// Урок 2. Задание 6
+//print ("Задание 6:")
+//homework_2_6(100)
+//print ("\n")
 
-// Урок 2. Задание 2
-print ("Задание 2:")
-homework_2_2(chislo: 27)
-print ("\n")
 
-// Урок 2. Задание 3
-print ("Задание 3:")
-print ("Массив: \(homework_2_3())")
-print ("\n")
 
-// Урок 2. Задание 4
-print ("Задание 4:")
-print ("Массив: \(homework_2_4(numbers: homework_2_3()))")
-print ("\n")
-
-// Урок 2. Задание 5
-print ("Задание 5:")
-print ("Массив: \(homework_2_5(50))")
-print ("\n")
-
-// Урок 2. Задание 6
-print ("Задание 6:")
-homework_2_6(100)
-print ("\n")
+//Урок 3
+homework_3()
